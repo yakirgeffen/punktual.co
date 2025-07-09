36cc6ba7b53ea76b07c961023a5d64fdbd2c5e2a
Your Name <your@email.com>
2025-07-09 19:55:55 +0300

    feat: Layout restructure and preview fixes

--- .history/src/components/EventCreator/sections/ButtonCustomization/index_20250623131505.tsx
+++
@@ -1,6 +0,0 @@
-// src/components/EventCreator/sections/ButtonCustomization/index.tsx
-
-export { default as LayoutStyleSection } from './LayoutStyleSection';
-export { default as ButtonStyleSection } from './ButtonStyleSection';
-export { default as ButtonColorSection } from './ButtonColorSection';
-export { default as AdditionalOptionsSection } from './AdditionalOptionsSection';
